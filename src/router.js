88fcd38842ce27155241601dedc9fe750d49c488
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-11-12 08:01:45 -0600

    modified vite config router.js

--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ import { useAuth } from './composables/useAuth'
 const { isAuthenticated } = useAuth()
 
 const routes = [
-  { path: '/homework5/', name: 'Home', component: HomePage },
-  { path: '/homework5/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/homework5/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/homework5/login', name: 'LoginPage', component: LoginPage },
-  { path: '/homework5/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
+  { path: '/homework5-api/', name: 'Home', component: HomePage },
+  { path: '/homework5-api/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/homework5-api/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/homework5-api/login', name: 'LoginPage', component: LoginPage },
+  { path: '/homework5-api/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
         
 ]
 
